e9b4b0fd8ea4dc15fa2840de2a87f0edb6a4173c
Eugene <yukovich01@mail.ru>
2023-06-20 11:23:34 +0300

    Seed Category Table.

--- BookShopWeb/Data/ApplicationDbContext.cs
+++ BookShopWeb/Data/ApplicationDbContext.cs
@@ -8,5 +8,16 @@ namespace BookShopWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var categories = new Category[]
+            {
+                new Category() { Id = 1, Name = "Business", DisplayOrder = 1 },
+                new Category() { Id = 2, Name = "Technologies", DisplayOrder = 2 },
+                new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
+            };
+            modelBuilder.Entity<Category>().HasData(categories);
+        }
     }
 }
