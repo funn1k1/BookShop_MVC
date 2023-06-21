72bb72277a94d2763901938eea38f2c15ae24121
Eugene <yukovich01@mail.ru>
2023-06-21 11:09:00 +0300

    Add Input Tag Helpers to Create.cshtml

--- BookShopWeb/Models/Category.cs
+++ BookShopWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookShopWeb.Models
 {
@@ -8,8 +9,11 @@ namespace BookShopWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
 
+        [Required]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
