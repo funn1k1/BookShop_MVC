d6a2a7a886775e41652a996c3a69413959ba98a8
Eugene <yukovich01@mail.ru>
2023-06-23 13:34:58 +0300

    Add BookShopWeb_Razor and Setup EF Core

---
+++ BookShopWeb_Razor/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookShopWeb_Razor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopWeb_Razor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var categories = new Category[]
+{
+                new Category() { Id = 1, Name = "Business", DisplayOrder = 1 },
+                new Category() { Id = 2, Name = "Technologies", DisplayOrder = 2 },
+                new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
+};
+            modelBuilder.Entity<Category>().HasData(categories);
+        }
+    }
+}
