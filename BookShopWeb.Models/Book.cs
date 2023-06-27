7b2bac7d89739e4080487344b2b4103721955e43
Eugene <yukovich01@mail.ru>
2023-06-28 00:22:33 +0300

    Add Book model

---
+++ BookShopWeb.Models/Book.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShopWeb.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(0.01, 100000)]
+        public decimal ListPrice { get; set; }
+
+        [Display(Name = "Discounted Price")]
+        [Range(0.01, 100000)]
+        public decimal? DiscountedPrice { get; set; }
+
+        [Display(Name = "Publication Date")]
+        [DataType(DataType.Date)]
+        public DateTime PublicationDate { get; set; }
+
+        [Display(Name = "Cover Image URL")]
+        [DataType(DataType.ImageUrl)]
+        public string CoverImageUrl { get; set; }
+
+        [Display(Name = "Available Quantity")]
+        [Range(0, int.MaxValue)]
+        public int AvailableQuantity { get; set; }
+    }
+}
